9636d41bb1e2e8f2ea53ac703dd3ff2237e6640b
NavyasriNagadevara <97424715+NavyasriNagadevara@users.noreply.github.com>
2022-06-28 22:35:13 +0530

    n = int(input()) x=2 c=0 d=0 p=0 while x<=n//2:     if n%x==0:         c+=1     x+=1 if c!=0:     print("Not Mega Prime") else:     while n:         if n%10==2 or n%10==3 or n%10==5 or n%10==7:             c+=1         d+=1         n//=10     if c==d:         print("Mega Prime")     else:         print("Not Mega Prime")

--- mega_prime.py
+++ mega_prime.py
@@ -1,26 +1,21 @@
-def is_prime(n):
-    c=0
-    flag=1
-    for i in range(1,n+1):
-        if n%i==0:
+n = int(input())
+x=2
+c=0
+d=0
+p=0
+while x<=n//2:
+    if n%x==0:
+        c+=1
+    x+=1
+if c!=0:
+    print("Not Mega Prime")
+else:
+    while n:
+        if n%10==2 or n%10==3 or n%10==5 or n%10==7:
             c+=1
-    if c==2:
-        return flag
-    else:
-        return 0
-n=int(input())
-if(is_prime(n)):
-    r=0
-    v=0
-    while(n>0):
-        d=n%10
-        if(is_prime(d)):
-            r+=1
-        v+=1
-        n=n//10
-    if v==r:
+        d+=1
+        n//=10
+    if c==d:
         print("Mega Prime")
     else:
         print("Not Mega Prime")
-else:
-    print("Not Mega Prime")
