092a3ce6ecc754e832b5e3d1ca1a9b44019f09dc
NavyasriNagadevara <97424715+NavyasriNagadevara@users.noreply.github.com>
2022-06-28 22:41:36 +0530

    n = int(input()) sq=n*n number=n d=0 temp=n while temp:     d+=1     temp//=10 rev=0 while d:     rev=rev*10+sq%10     sq//=10     d-=1 rev2=0 while rev:     rev2=rev2*10+rev%10     rev//=10 if rev2==number:     print("Automorphic Number") else:     print("Not an Automorphic Number")

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,16 +1,21 @@
-n=int(input())
-a=10
-sq=0
-i=0
-while True:
-    if n<a:
-        sq=n**2
-        d=sq%a
-        if n==d:
-            print("Automorphic Number")
-            break
-        else:
-            print("Not an Automorphic Number")
-            break
-    a=a*10
-    i+=1
+n = int(input())
+sq=n*n
+number=n
+d=0
+temp=n
+while temp:
+    d+=1
+    temp//=10
+rev=0
+while d:
+    rev=rev*10+sq%10
+    sq//=10
+    d-=1
+rev2=0
+while rev:
+    rev2=rev2*10+rev%10
+    rev//=10
+if rev2==number:
+    print("Automorphic Number")
+else:
+    print("Not an Automorphic Number")
