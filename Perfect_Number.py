36ed1b5f1ac4d22d7ebc2733996ac0cbe0fede68
NavyasriNagadevara <97424715+NavyasriNagadevara@users.noreply.github.com>
2022-06-28 22:23:38 +0530

    n = int(input()) sum=0 x=1 while x<=n//2:     if n%x==0:         sum+=x     x+=1 if sum==n:     print(True) else:     print(False)

--- Perfect_Number.py
+++ Perfect_Number.py
@@ -1,10 +1,11 @@
-a=int(input())
-temp=a
-c=0
-for i in range(1,a):
-    if a%i==0:
-        c=c+i
-if temp==c:
-    print("True")
+n = int(input())
+sum=0
+x=1
+while x<=n//2:
+    if n%x==0:
+        sum+=x
+    x+=1
+if sum==n:
+    print(True)
 else:
-    print("False")
+    print(False)
