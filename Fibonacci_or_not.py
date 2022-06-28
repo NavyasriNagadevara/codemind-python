1d8cc20f846fecd7fda9f3f175242c96b2b4c3b5
NavyasriNagadevara <97424715+NavyasriNagadevara@users.noreply.github.com>
2022-06-28 22:51:28 +0530

    def fib(n):     a,b=0,1     if n==a or n==b:         print(True)     else:         c=a+b         a=b         b=c         while c<n:             c=a+b             a=b             b=c         if c==n:             print(True)         else:             print(False)
    n=int(input())
    fib(n)

--- Fibonacci_or_not.py
+++ Fibonacci_or_not.py
@@ -1,19 +1,19 @@
-n = int(input())
-first_number=0
-second_number=1
-next_number=0
-if n==1 or n==1:
-    print(True)
-else:
-    c=0
-    while next_number<=n:
-        next_number=first_number+second_number
-        first_number=second_number
-        second_number=next_number
-        if second_number==n:
-            c+=1
-            break
-    if c==0:
-        print(False)
-    else:
+def fib(n):
+    a,b=0,1
+    if n==a or n==b:
         print(True)
+    else:
+        c=a+b
+        a=b
+        b=c
+        while c<n:
+            c=a+b
+            a=b
+            b=c
+        if c==n:
+            print(True)
+        else:
+            print(False)
+
+n=int(input())
+fib(n)
