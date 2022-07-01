d4d6118a411a544d6a4a5c8412a6246afadb7aa1
NavyasriNagadevara <97424715+NavyasriNagadevara@users.noreply.github.com>
2022-07-01 11:15:21 +0530

    s=input() s=s.lower() k=list(set(s)) k.sort() for i in k:     if i==" ":         del k[k.index(i)] print("".join(k))

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,17 +1,8 @@
-n = input()
-n = n.lower()
-n = n.replace(" ","")
-s = []
-for i in range(0,len(n)):
-    c = n.count(n[i])
-    if c==1:
-        s.append(n[i])
-s.sort()
-s = str(s)
-s = s.replace(",","")
-s = s.replace("[","")
-s = s.replace("]","")
-s = s.replace(",","")
-s = s.replace("'","")
-s = s.replace(" ","")
-print(s)
+s=input()
+s=s.lower()
+k=list(set(s))
+k.sort()
+for i in k:
+    if i==" ":
+        del k[k.index(i)]
+print("".join(k))
