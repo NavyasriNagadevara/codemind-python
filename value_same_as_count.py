52646b6d3013fe6b6db37a9d629acf7fed36ef6c
NavyasriNagadevara <97424715+NavyasriNagadevara@users.noreply.github.com>
2022-09-19 23:14:22 +0530

    x=int(input()) l=list(map(int,input().split())) k=set(l) c=0 for i in k:     if l.count(i)==i:         c+=1 print(c)

--- value_same_as_count.py
+++ value_same_as_count.py
@@ -1,9 +1,9 @@
-n=int(input())
-a=list(map(int,input().split()))
-b=[]
+x=int(input())
+l=list(map(int,input().split()))
+k=set(l)
 c=0
-for i in a:
-    if i not in b and a.count(i)==i:
-     c+=1
-     b.append(i)
+for i in k:
+    if l.count(i)==i:
+        c+=1
 print(c)
+    
