2ea1c488d7c554f978fecd6a89cac1cdd0ac9a4f
NavyasriNagadevara <97424715+NavyasriNagadevara@users.noreply.github.com>
2022-09-28 00:13:08 +0530

    def is_it_ambient(m,n):     sum=0     for i in range(1,m):         if m%i==0:             sum=sum+i     if sum==n:         return True     else:         return False m=int(input()) n=int(input()) if(is_it_ambient(m,n)):     m,n=n,m     if(is_it_ambient(m,n)):         print('Amicable')     else:         print('Not Amicable') else:     print('Not Amicable')

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,12 +1,19 @@
-a = int(input())
-b = int(input())
-x = 1
-sum = 0
-while x<=a//2:
-    if a%x==0:
-        sum+=x
-    x+=1
-if sum==b:
-    print("Amicable")
+def is_it_ambient(m,n):
+    sum=0
+    for i in range(1,m):
+        if m%i==0:
+            sum=sum+i
+    if sum==n:
+        return True
+    else:
+        return False
+m=int(input())
+n=int(input())
+if(is_it_ambient(m,n)):
+    m,n=n,m
+    if(is_it_ambient(m,n)):
+        print('Amicable')
+    else:
+        print('Not Amicable')
 else:
-    print("Not Amicable")
+    print('Not Amicable')
