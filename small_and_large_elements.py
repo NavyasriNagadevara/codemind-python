f3c55789b7095f80823a549ee588c2187163c5ed
NavyasriNagadevara <97424715+NavyasriNagadevara@users.noreply.github.com>
2022-10-07 18:59:05 +0530

    a=input() b=[] a=a.split() for i in a:     k=min(i)     g=max(i)     b.append(k)     b.append(g) print(b[0],b[-1])

--- small_and_large_elements.py
+++ small_and_large_elements.py
@@ -1,6 +1,9 @@
-n = input()
-m = n.split(" ")
-if len(m)==2:
-    print(min(m[0]),max(m[1]))
-else:
-    print(min(m[0]),max(m[len(m)-2]))
+a=input()
+b=[]
+a=a.split()
+for i in a:
+    k=min(i)
+    g=max(i)
+    b.append(k)
+    b.append(g)
+print(b[0],b[-1])
