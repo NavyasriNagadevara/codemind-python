0022ebd0c30ecd614eff3887097854abfd64836e
NavyasriNagadevara <97424715+NavyasriNagadevara@users.noreply.github.com>
2022-10-07 19:35:45 +0530

    a=input() a=a.lower() c=0 for i in a:     if i in "abcdefghijklmnopqrstuvwxyz":         c=c+1 if c>=26:     print(True) else:     print(False)

--- Pangram_or_not.py
+++ Pangram_or_not.py
@@ -1,10 +1,10 @@
-s = input().lower()
-a = ''
-for i in s:
-    if i not in a:
-        a+=i
-a = a.replace(" ","")
-if len(a)==26:
+a=input()
+a=a.lower()
+c=0
+for i in a:
+    if i in "abcdefghijklmnopqrstuvwxyz":
+        c=c+1
+if c>=26:
     print(True)
 else:
     print(False)
