27f05e61ce66dfaee31f0a6af6fef80251c4ba08
NavyasriNagadevara <97424715+NavyasriNagadevara@users.noreply.github.com>
2022-10-07 18:52:24 +0530

    n=input() n=n.split() c=0 b='AEIOUaeiou' d="bcdfghjklmnpqrstvwxyz" for i in n:     if i[0] in b and i[-1] in d:         c=c+1 print(c)

--- count_words.py
+++ count_words.py
@@ -1,9 +1,9 @@
-n = input()
-n = n.lower()
-n = n.split(" ")
-count = 0
-for i in range(len(n)):
-    a = " ".join(n[i])
-    if a[0] in 'aeiou' and a[len(a)-1] in 'qwrtypsdfghjklzxcvbnm':
-        count += 1
-print(count)
+n=input()
+n=n.split()
+c=0
+b='AEIOUaeiou'
+d="bcdfghjklmnpqrstvwxyz"
+for i in n:
+    if i[0] in b and i[-1] in d:
+        c=c+1
+print(c)
