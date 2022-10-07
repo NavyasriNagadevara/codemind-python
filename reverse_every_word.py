f3bed8c826dd3d030d5dc7798ee943368c3aef8b
NavyasriNagadevara <97424715+NavyasriNagadevara@users.noreply.github.com>
2022-10-07 18:07:41 +0530

    n=input() n=n.split() b=[] for i in n:     i=i[::-1]     b.append(i) print(*b)

--- reverse_every_word.py
+++ reverse_every_word.py
@@ -1,6 +1,7 @@
-n = input()
-n = n.split(" ")
-for j in range(len(n)):
-    for i in range(len(n[j])-1,-1,-1):
-        print(n[j][i],end="")
-    print(end=" ")
+n=input()
+n=n.split()
+b=[]
+for i in n:
+    i=i[::-1]
+    b.append(i)
+print(*b)
