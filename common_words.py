1b31acd34f4da5a411b941c523e210e3dde417f6
NavyasriNagadevara <97424715+NavyasriNagadevara@users.noreply.github.com>
2022-10-07 19:20:24 +0530

    a=input() a=a.lower() b=input() b=b.lower() g=[] c=0 a=a.split() b=b.split() for j in b:     for i in a:         if j==i:             g.append(j) print(*g)

--- common_words.py
+++ common_words.py
@@ -1,11 +1,13 @@
-s1=input()
-s2=input()
-s1=s1.lower()
-s2=s2.lower()
-a=list(s1.split(" "))
-b=list(s2.split(" "))
-c=[]
-for i in b:
-    if i in a and i not in c:
-        c.append(i)
-print(" ".join(c))
+a=input()
+a=a.lower()
+b=input()
+b=b.lower()
+g=[]
+c=0
+a=a.split()
+b=b.split()
+for j in b:
+    for i in a:
+        if j==i:
+            g.append(j)
+print(*g)
