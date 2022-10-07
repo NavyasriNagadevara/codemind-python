483dcf2a5f2e209dff544f5501dcb2dac1a1a324
NavyasriNagadevara <97424715+NavyasriNagadevara@users.noreply.github.com>
2022-10-07 19:34:17 +0530

    a=input() a=a.lower() temp=a k=a[::-1] l="".join(k) if temp!=l:     print("False") else:     print("True")

--- Palindrome_or_not.py
+++ Palindrome_or_not.py
@@ -1,29 +1,9 @@
-n = input()
-#print(n)
-i = 0
-j = len(n)-1
-count = 0
-n = n.lower()
-#print(n)
-if len(n)%2!=0:
-    while i<=j:
-        if n[i]==n[j]:
-            count += 1
-            #print(n[i],n[j])
-        i += 1
-        j -= 1
-    if count==len(n)//2+1:
-        print(True)
-    else:
-        print(False)
+a=input()
+a=a.lower()
+temp=a
+k=a[::-1]
+l="".join(k)
+if temp!=l:
+    print("False")
 else:
-    while i<j:
-        if n[i]==n[j]:
-            count += 1
-            #print(n[i],n[j])
-        i += 1
-        j -= 1
-    if count==len(n)//2:
-        print(True)
-    else:
-        print(False)
+    print("True")
