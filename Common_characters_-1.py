de42f731c814ee1ed0306860a9c0af94594c8f88
NavyasriNagadevara <97424715+NavyasriNagadevara@users.noreply.github.com>
2022-10-07 19:29:49 +0530

    n=input() n=n.lower() n=n.split() c=0 s=n[0] for i in s:     for j in n:         if i in j:            # print(i)             continue         else:             #print(j)             break     else:         print(i,end='')         c=c+1 if c==0:     print(-1)

--- Common_characters_-1.py
+++ Common_characters_-1.py
@@ -1,16 +1,18 @@
-s = input().lower()
-s = s.split(" ")
-a = s[0]
-c = 0
-x = ''
-for i in a:
-    count = 0
-    for j in range(1,len(s)):
-        if i in s[j]:
-            count+=1
-    if count==len(s)-1:
-        x+=i
-if len(x)==0:
-    print("-1")
-else:
-    print(x)
+n=input()
+n=n.lower()
+n=n.split()
+c=0
+s=n[0]
+for i in s:
+    for j in n:
+        if i in j:
+           # print(i)
+            continue
+        else:
+            #print(j)
+            break
+    else:
+        print(i,end='')
+        c=c+1
+if c==0:
+    print(-1)
