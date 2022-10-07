007050571c5775d2f5d064cc590ca798a8cb4a3a
NavyasriNagadevara <97424715+NavyasriNagadevara@users.noreply.github.com>
2022-10-07 19:35:00 +0530

    a=input() b=input() a=a.lower() b=b.lower() a=set(a) b=set(b) if a==b:     print(True) else:     print(False)

--- Anagrams.py
+++ Anagrams.py
@@ -1,15 +1,10 @@
-a = input()
-b = input()
-a,b = a.lower(),b.lower()
-c_a = 0
-c_b = 0
-for i in a:
-    if i in b:
-        c_a += 1
-for i in b:
-    if i in a:
-        c_b += 1
-if c_b==len(a) and c_a==len(b):
+a=input()
+b=input()
+a=a.lower()
+b=b.lower()
+a=set(a)
+b=set(b)
+if a==b:
     print(True)
 else:
     print(False)
