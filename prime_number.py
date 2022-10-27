1eccae684ecdf43d02567b98e0782db3bf6e4053
NavyasriNagadevara <97424715+NavyasriNagadevara@users.noreply.github.com>
2022-10-27 10:53:40 +0530

    n=int(input()) c=0 for i in range(1,n+1):     if(n%i==0):         c+=1 if(c==2):     print("prime") else:     print("not a prime")

--- prime_number.py
+++ prime_number.py
@@ -1,10 +1,9 @@
-a=int(input())
+n=int(input())
 c=0
-for i in range(1,int(pow(a*a,0.5))+1):
-    if a%i==0:
-        c=c+1
-if c==2:
+for i in range(1,n+1):
+    if(n%i==0):
+        c+=1
+if(c==2):
     print("prime")
 else:
     print("not a prime")
-    
